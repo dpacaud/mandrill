8d27589b38e22bf543a66429bd8be22b85bab735
Jim Schindler <jamesschindler21@gmail.com>
2014-03-04 11:51:07 -0500

    Add message field to sendResponse object.

--- src/groovy/org/grails/mandrill/SendResponse.groovy
+++ src/groovy/org/grails/mandrill/SendResponse.groovy
@@ -3,6 +3,7 @@ import grails.converters.JSON
 
 class SendResponse {
     String status
+    String message
     String email
     String id
     String rejectReason
@@ -11,6 +12,7 @@ class SendResponse {
     SendResponse(data) {
         data = JSON.parse(data).first()
         this.status = data?.status
+        this.message = data?.message
         this.rejectReason = data?.reject_reason
         this.email = data?.email
         this.id = data?._id
