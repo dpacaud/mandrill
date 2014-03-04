dbed14ec180f7d44886858184a70dca0c60d88df
Damien Pacaud <damien.pacaud@carboatmedia.fr>
2014-03-04 16:15:07 +0100

    upgrade to V0.2 with pull requests                      Delete unneeded files

--- MandrillGrailsPlugin.groovy
+++ MandrillGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class MandrillGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
