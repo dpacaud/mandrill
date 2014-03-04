be5bcaad9fcb8cd1f6ff788985afcbd4a906c5a5
Jim Schindler <jamesschindler21@gmail.com>
2014-03-04 10:27:00 -0500

    Add rejectReason to sendResponse.

--- src/groovy/org/grails/mandrill/SendResponse.groovy
+++ src/groovy/org/grails/mandrill/SendResponse.groovy
@@ -4,11 +4,13 @@ class SendResponse {
     String status
     String email
     String id
+    String rejectReason
     boolean success
 
     SendResponse(data) {
         data = data.first()
         this.status = data?.status
+        this.rejectReason = data?.reject_reason
         this.email = data?.email
         this.id = data?._id
         success = ['sent', 'queued', 'scheduled'].contains(this.status)
